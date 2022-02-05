9c070009b5d7120fb807466fccf85cb7f7c390e6
Wilder Lopes <wilder@ogarantia.com>
2022-02-05 21:15:05 +0100

    Bug fix in ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -11,16 +11,16 @@
 sudo apt-get update && sudo apt-get install -y \
 	vim \
 	git \
-    curl \
-    build-essential \
-    cmake
+	curl \
+	build-essential \
+	cmake
 
 # Clone linux scripts from Github -------------------------------------------------------
 cd /home/wilder && \
-    git clone git@github.com:wilderlopes/my-scripts.git \
+    git clone git@github.com:wilderlopes/my-scripts.git && \
     git clone git@github.com:wilderlopes/my-dotfiles.git
 
-export 'PATH="/home/wilder/my-scripts":$PATH' >> /home/wilder/.bashrc
+echo 'export PATH="/home/wilder/my-scripts":$PATH' >> /home/wilder/.bashrc
 
 # Copy dotfiles to home folder ----------------------------------------------------------
 cp my-dotfiles/.vimrc .
@@ -31,12 +31,13 @@ curl https://get.docker.com | sh && sudo systemctl --now enable docker
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
     && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo t    ee /etc/apt/sources.list.d/nvidia-docker.list
+    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
 sudo apt-get update && sudo apt-get install -y nvidia-docker2
 sudo systemctl restart docker
 
 # Add user to docker group to enable using docker without sudo --------------------------
-sudo usermod -aG docker $USER
-newgrp docker
+sudo usermod -aG docker $USER && newgrp docker
+
+echo ">>> Run Docker hello-world"
 docker run hello-world
